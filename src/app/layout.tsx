1bdaddb0eab42471434dbf64cfdc26cc2a05339c
seanchen <270061968@qq.com>
2026-07-19 18:06:58 +0800

    style: add light mode css variables

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body className={`${geistSans.variable} ${dmMono.variable} antialiased`}>
         <NuqsAdapter>{children}</NuqsAdapter>
         <Toaster />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,38 +1,42 @@
-import type { Config } from 'tailwindcss'
-import tailwindcssAnimate from 'tailwindcss-animate'
+import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
 
 export default {
-  darkMode: ['class'],
+  darkMode: ["class"],
+
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
   theme: {
     extend: {
       colors: {
-        primary: '#FAFAFA',
-        primaryAccent: '#18181B',
-        brand: '#FF4017',
+        primary: "rgb(var(--color-primary) / <alpha-value>)",
+        primaryAccent: "rgb(var(--color-primary-accent) / <alpha-value>)",
+        brand: "rgb(var(--color-brand) / <alpha-value>)",
         background: {
-          DEFAULT: '#111113',
-          secondary: '#27272A'
+          DEFAULT: "rgb(var(--color-background) / <alpha-value>)",
+          secondary:
+            "rgb(var(--color-background-secondary) / <alpha-value>)",
         },
-        secondary: '#f5f5f5',
-        border: 'rgba(var(--color-border-default))',
-        accent: '#27272A',
-        muted: '#A1A1AA',
-        destructive: '#E53935',
-        positive: '#22C55E'
+        secondary: "rgb(var(--color-secondary) / <alpha-value>)",
+        border: "rgb(var(--color-border-default) / <alpha-value>)",
+        accent: "rgb(var(--color-accent) / <alpha-value>)",
+        muted: "rgb(var(--color-muted) / <alpha-value>)",
+        destructive: "rgb(var(--color-destructive) / <alpha-value>)",
+        positive: "rgb(var(--color-positive) / <alpha-value>)",
       },
       fontFamily: {
-        geist: 'var(--font-geist-sans)',
-        dmmono: 'var(--font-dm-mono)'
+        geist: "var(--font-geist-sans)",
+        dmmono: "var(--font-dm-mono)",
       },
       borderRadius: {
-        xl: '10px'
-      }
-    }
+        xl: "10px",
+      },
+    },
   },
-  plugins: [tailwindcssAnimate]
-} satisfies Config
+
+  plugins: [tailwindcssAnimate],
+} satisfies Config;
